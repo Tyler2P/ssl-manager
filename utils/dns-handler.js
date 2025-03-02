3cb3111b511f11ab6580515c4007a75f3041306e
Tyler <tylerrp06@gmail.com>
2025-03-02 18:45:48 +0000

    Update dns-handler.js
    * Released the DB connection back to the pool when finished
    * Changed required parameters for global functions
    * Added support for a primary domain
    * Made error messages more consistent

--- utils/dns-handler.js
+++ utils/dns-handler.js
@@ -1,51 +1,56 @@
 const axios = require("axios");
-const { getDB } = require("./functions");
+const { getDB, formatLog } = require("./functions");
 
 module.exports = {
-  updateDNS: async function(db, dnsProfile, record, value) {
-    let dbProvided = true;
-    // Ensure a database connection can be found
-    if (!db) {
-      db = await getDB();
-      dbProvided = false;
-    }
-    if (!db) return false;
+  /**
+   * Update DNS records for a domain
+   * @param {object | string} dnsProfile The DNS Profile to update
+   * @param {string} record The name of the DNS record
+   * @param {string} value The value of the DNS record
+   * @param {string?} primaryDomainName The primary domain name for the SSL certificate
+   * @returns Whether the function was successful
+   */
+  updateDNS: async function(dnsProfile, record, value, primaryDomainName) {
+    // Get a database connection
+    const db = await getDB();
 
     // Fetch the DNS profile
     if (typeof dnsProfile === "string") {
       [dnsProfile] = await db.query("SELECT * FROM dns_profiles WHERE id = ?", [dnsProfile]);
-      if (!dnsProfile[0])
+      if (!dnsProfile[0]) {
+        db.release();
         return false;
+      }
     }
-    if (!dnsProfile)
+    if (!dnsProfile) {
+      db.release();
       return false;
+    }
 
     // Ensure the DNS profile has required parameters
-    if (!dnsProfile["zone_id"])
+    if (!dnsProfile["zone_id"]) {
+      db.release();
       return false;
-    if (!dnsProfile["api_key"])
+    }
+    if (!dnsProfile["api_key"]) {
+      db.release();
       return false;
-
-    console.log("DNS Value (2): " + value);
+    }
   
-    // const url = `https://api.cloudflare.com/client/v4/zones/${dnsProfile["zone_id"]}/dns_records`;
     let url = value === null ? (dnsProfile["api_delete_url"] || dnsProfile["api_create_url"]) : dnsProfile["api_create_url"];
     let headers = {
       "Content-Type": "application/json",
       "Authorization": `Bearer ${dnsProfile["api_key"]}`
     }
-    console.log("Headers:");
-    console.log(headers);
 
     value = [null, "", " "].includes(value) ? null : value;
 
     // Replace variables in URL
-    url = url.replaceAll("{domain.name}", );
+    if (primaryDomainName)
+      url = url.replaceAll("{domain.name}", primaryDomainName);
     url = url.replaceAll("{zone.id}", dnsProfile["zone_id"]);
     url = url.replaceAll("{record.name}", record);
     url = url.replaceAll("{record.value}", value);
-
-    console.log(url);
 
     // Update the DNS record
     try {
@@ -66,17 +71,21 @@ module.exports = {
         }
       });
 
-      console.log(response.data);
-
-      if (!(response.status || 0).toString().startsWith("2"))
+      if (!(response.status || 0).toString().startsWith("2")) {
+        db.release();
         return false;
+      }
     } catch (err1) {
-      // console.log(err1);
+      console.log(formatLog("ERROR", `Something went wrong updating DNS in profile ${dnsProfile.id}, domain: ${domains[0]}:`));
       console.log(err1?.response?.data);
-      console.log(err1?.response?.data?.errors);
+      db.release();
       return false;
     }
-    console.log("DNS has returned true!");
+
+    db.release();
     return true;
   },
+  dnsCleanup: async function(dnsProfile) {
+
+  }
 }
